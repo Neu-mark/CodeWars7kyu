e117d19ca4b7478009fe19ee7c4191d7ccdbec88
Mark Neumann <mark.neumann.sm@gmail.com>
2024-07-23 10:59:02 +0300

    feat: Perimeter sequence.js

---
+++ Perimeter sequence.js
@@ -0,0 +1,17 @@
+/*
+Perimeter sequence
+
+DESCRIPTION:
+Perimeter sequence
+The first three stages of a sequence are shown.
+
+blocks
+
+The blocksize is a by a and a ≥ 1.
+
+What is the perimeter of the nth shape in the sequence (n ≥ 1) ?
+SOLUTION: 
+*/
+function perimeterSequence(a, n) {
+  return 4 * a * n;
+}
