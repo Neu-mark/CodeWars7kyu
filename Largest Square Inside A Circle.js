8c48e18911d18ce70715f611d009cf515b72238a
Mark Neumann <mark.neumann.sm@gmail.com>
2024-07-23 11:02:03 +0300

    feat: Largest Square Inside A Circle.js

---
+++ Largest Square Inside A Circle.js
@@ -0,0 +1,11 @@
+/*
+Largest Square Inside A Circle
+
+
+DESCRIPTION:
+Determine the area of the largest square that can fit inside a circle with radius r.
+SOLUTION: 
+*/
+function areaLargestSquare(r) {
+  return  2*r*r;
+}
